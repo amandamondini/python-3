25f07572bb1fdb0dd7490a90ca45f54238bee9fd
Amanda Mondini <amandamondini4@gmail.com>
2023-09-20 08:14:39 -0400

    functions finished

--- functions.py
+++ functions.py
@@ -21,3 +21,59 @@ def generate_trip_instructions(location):
     print('You can use the public subway system to get to', location)
 
 generate_trip_instructions('Grand Central Station')
+
+# Examples:
+
+# converts farenheit to celsius
+def f_to_c(f_temp):
+    c_temp = (f_temp-32)*(5/9)
+    return c_temp
+
+f100_in_celsius = f_to_c(100)
+print(f100_in_celsius)
+
+#converts celsius to fahrenheit
+def c_to_f(c_temp):
+    f_temp = (c_temp)*(9/5) +32
+    return f_temp
+
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+
+# another example calculating force 
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+def get_force(mass, acceleration):
+    return mass*acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+print('The GE train supplies', train_force, 'Newtons of force')
+
+
+#?Function with default parameter/argument 
+    # will use default parameter if no argument is passed. If an argument is passed it will override the default parameter
+
+def findvolume(length=1, width=1, depth=1):
+    print("Length = " + str(length))
+    print("Width = " + str(width))
+    print("Depth = " + str(depth))
+    return length * width * depth
+
+print('Default Parameter Example:')
+findvolume(2,1)
+
+#?Functions with keyword arguments
+    # Allows for arguments to be passed in any order 
+    
+def findvolume(length=1, width=1, depth=1):
+    print("Length = " + str(length))
+    print("Width = " + str(width))
+    print("Depth = " + str(depth))
+    return length * width * depth
+
+print('keyword argument example:')
+findvolume(length=5, depth=2, width=4) # overrides the default parameter and is given in a different order than the parameters of the function
